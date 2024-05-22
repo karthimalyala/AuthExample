9b051558adcee0f0a9e17c0c8267ef26c871275d
Karthi Teja Malyala <84483984+karthimalyala@users.noreply.github.com>
2024-05-22 11:28:08 +0530

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,7 +1,9 @@
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const tenantId = process.env.REACT_APP_TENANT_ID;
 export const msalConfig = {
     auth: {
-        clientId: "99339a78-fbb1-4ad8-b9da-7c1ea226fcc8",
-        authority: "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47",
+        clientId: clientId,
+        authority: "https://login.microsoftonline.com/${tenantId}",
         redirectUri: "http://localhost:3000",
     },
     cache: {
